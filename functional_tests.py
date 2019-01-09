5b1046cf6f859423dd8b8a988d0d02f14b80e3d6
Vincent Smith <VinceS22@gmail.com>
2019-01-09 16:15:28 -0500

    Functional tests now check for header tag. Unit test now checks to see if home page is an actual html document with a title.

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase): 
@@ -15,16 +17,33 @@ class NewVisitorTest(unittest.TestCase):
         # We want to be able to access a to-do list on the home page
         self.assertIn('To-Do', self.browser.title)
         
-        self.fail("Browser title was " + browser.title)
-
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+        
         # We want to be able to add a To-Do item immediately
+        
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         # A textbox exists where we can put the information for a to do item.
+        
+        inputbox.send_keys('Buy acme products')
 
         # Upon adding it to the list, it should be in a format "'([0-9][:])+ ([a-z]|[A-Z]|[0-9]|\s)+'" For Example: 1. Write annoying regex
-
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy acme products')
+        )
+        
         # Make sure the textbox still exists after adding an item
-
+        self.fail('Finish the test!')
         # Add another item
 
         # Check existence of both items now.
