0d63586c7008ad46d9ce0671642199f49aef6ad8
Vincent Smith <VinceS22@gmail.com>
2019-01-10 18:25:51 -0500

    Adding functional tests that check multiple items in the list now. Refactored to-do item input logic to a method so that we can just send the text we want. Also added simple HTML method checks for home page

--- functional_tests.py
+++ functional_tests.py
@@ -20,29 +20,15 @@ class NewVisitorTest(unittest.TestCase):
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
         
-        # We want to be able to add a To-Do item immediately
+        # We want to be able to add a To-Do items immediately
+        self.enter_input_into_new_item_textbox('Buy acme products')
+        self.enter_input_into_new_item_textbox('Catch road runner')
         
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(
-            inputbox.get_attribute('placeholder'),
-            'Enter a to-do item'
-        )
-
-        # A textbox exists where we can put the information for a to do item.
-        
-        inputbox.send_keys('Buy acme products')
-
-        # Upon adding it to the list, it should be in a format "'([0-9][:])+ ([a-z]|[A-Z]|[0-9]|\s)+'" For Example: 1. Write annoying regex
-        inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
         
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy acme products' for row in rows),
-            "New to-do item did not appear in the table" 
-        )
-        
+        self.assertIn('1: Buy acme products', [row.text for row in rows])
+        self.assertIn('2: Catch road runner', [row.text for row in rows])
         # Make sure the textbox still exists after adding an item
         self.fail('Finish the test!')
         # Add another item
@@ -54,6 +40,21 @@ class NewVisitorTest(unittest.TestCase):
         # URL must be in format that contains all to-do information
 
         # Verify that new link contains the previous items.
+
+    def enter_input_into_new_item_textbox(self, item_text):
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        # A textbox exists where we can put the information for a to do item.
+        
+        inputbox.send_keys(item_text)
+
+        # Upon adding it to the list, it should be in a format "'([0-9][:])+ ([a-z]|[A-Z]|[0-9]|\s)+'" For Example: 1. Write annoying regex
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
